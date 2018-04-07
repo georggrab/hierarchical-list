d2094c093d38936892e587d7ffe2e657a4e49a29
talkdirty <talkdirty@users.noreply.github.com>
2018-04-07 22:51:30 +0200

    TDD: add test for children rendering: there have to be multiple .HierarchyList's.

--- src/components/HierarchyList/HierarchyList.test.js
+++ src/components/HierarchyList/HierarchyList.test.js
@@ -5,10 +5,9 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import HierarchyList from '.';
-import { flatHierarchy } from 'testAssets/HierarchyList';
+import { flatHierarchy, singleChildHierarchy } from 'testAssets/HierarchyList';
 
 Enzyme.configure({ adapter: new Adapter() });
-
 
 it('renders flat hierarchy list without error', () => {
   const div = document.createElement('div');
@@ -25,4 +24,9 @@ it('fails if hierarchy prop is missing', () => {
 it('produces correct amount of rows', () => {
   const hList = shallow(<HierarchyList hierarchy={flatHierarchy} />);
   expect(hList.find('section').length).toBe(4);
+});
+
+it('produces a second Hierarchy when children are supplies', () => {
+  const hList = shallow(<HierarchyList hierarchy={singleChildHierarchy} />);
+  expect(hList.find('.HierarchyList').length).toBe(2);
 });

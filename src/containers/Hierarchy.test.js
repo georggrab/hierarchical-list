b09acd89e1666f64fe54ffff9a46cf05b76f8ce1
talkdirty <talkdirty@users.noreply.github.com>
2018-04-09 03:12:01 +0200

    Add test for row expansion

--- src/containers/Hierarchy.test.js
+++ src/containers/Hierarchy.test.js
@@ -4,9 +4,23 @@ import { mount } from 'enzyme';
 
 import Hierarchy from 'containers/Hierarchy';
 import { commonTestSetup } from 'commonTest';
+import { multiNestedChildHierarchyState } from 'testAssets/HierarchyList';
+import { EXPAND_ROW } from 'state/ducks/hierarchy/index';
 
-const { store } = commonTestSetup()
+const { store } = commonTestSetup(multiNestedChildHierarchyState)
 
-it('should mount', () => {
-    const el = mount(<Hierarchy store={store}/>)
+it('should dispatch EXPAND_ROW', () => {
+    const el = mount(<Hierarchy 
+        store={store}/>)
+    expect(el.find('.HierarchyList').length).toBe(1);
+    el.find('.HierarchyList-RowExpand p')
+        .simulate('click')
+    expect(store.getActions())
+        .toEqual([
+            { type: EXPAND_ROW,
+              hierarchyIndex: 0,
+              rowIndex: 0,
+              destinationState: true,
+            }
+        ])
 })
